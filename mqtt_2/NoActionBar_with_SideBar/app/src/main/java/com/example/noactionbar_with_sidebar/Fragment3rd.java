c3ea34068c1864908efb49018e75aa2ff1234a0f
eruns001 <50986002+eruns001@users.noreply.github.com>
2021-07-22 12:01:57 +0900

    chart No2

--- mqtt_2/NoActionBar_with_SideBar/app/src/main/java/com/example/noactionbar_with_sidebar/Fragment3rd.java
+++ mqtt_2/NoActionBar_with_SideBar/app/src/main/java/com/example/noactionbar_with_sidebar/Fragment3rd.java
@@ -1,5 +1,6 @@
 package com.example.noactionbar_with_sidebar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,14 @@ import android.widget.GridLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment3rd extends Fragment {
 
@@ -26,16 +35,47 @@ public class Fragment3rd extends Fragment {
 
         GridLayout layout = view.findViewById(R.id.layout_correction);
 
+        boolean chartBool = false;
+
+        LineChart lineChart = view.findViewById(R.id.chart);
+
+        List<Entry> entries = new ArrayList<>();
+
         for(Integer i = 1; i <= 30; i++){
             //testLI
-            CorrectionItem correctionItem = new CorrectionItem(getActivity());
+            LogItem logItem = new LogItem(getActivity());
 
-            correctionItem.setChString("ch"+i);
-            correctionItem.setValue(i+30);
+            logItem.setChString("ch"+i);
+            logItem.setValue(i+30);
 
             //add child
-            layout.addView(correctionItem);
+            layout.addView(logItem);
+
+            //add chart data
+            if(chartBool){
+                entries.add(new Entry(i, i+1));
+            }
+            else {
+                entries.add(new Entry(i, i-1));
+            }
+            chartBool = !chartBool;
         }
+
+        LineDataSet lineDataSet = new LineDataSet(entries, "속성명1");
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setCircleRadius(6);
+        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        lineDataSet.setDrawHighlightIndicators(false);
+        lineDataSet.setDrawValues(false);
+
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+
+
         //실행내용
     }
 }
